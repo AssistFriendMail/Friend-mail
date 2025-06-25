ac8b8d75ca941cfa213e48929a389af7653f2bd3
hiagoCf19 <hiagoferreira@id.uff.br>
2025-06-24 21:58:10 -0300

    initial

---
+++ src/main/java/com/friend/mail/demo/FriendMail.java
@@ -0,0 +1,13 @@
+package com.friend.mail.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendMail {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendMail.class, args);
+	}
+
+}
